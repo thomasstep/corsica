a9ae8f7b6c0754ec42f1c01f0e156c66f8a87e6f
thomasstep <thomasstep@outlook.com>
2018-07-15 11:06:44 -0700

    Everything is working but doesn't look pretty. Added a constant for my IP that will have to be changed.

--- src/modules/movie-browser/movie-browser.helpers.js
+++ src/modules/movie-browser/movie-browser.helpers.js
@@ -1,14 +1,15 @@
-const TMDB_IMAGE_BASE_URL = (width = 300) =>
-  `http://172.24.16.147:3001/image`;
+import { HOST_IP } from "../../common/constant.js";
+
+const MOVIE_DB_BASE_URL = "http://" + HOST_IP + ":3001/image";
 
 export const updateMoviePictureUrls = (movieResult, width = 300) => {
   if (movieResult) {
     return {
       ...movieResult,
-      backdrop_path: `${TMDB_IMAGE_BASE_URL(width)}${
+      backdrop_path: `${MOVIE_DB_BASE_URL}${
         movieResult.backdrop_path
       }`,
-      poster_path: `${TMDB_IMAGE_BASE_URL(width)}${movieResult.poster_path}`
+      poster_path: `${MOVIE_DB_BASE_URL}${movieResult.poster_path}`
     };
   }
   return {};
